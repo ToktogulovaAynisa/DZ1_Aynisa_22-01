7cdb2be81ca96ce0b76d79b06e7b9578c4a68a8d
ToktogulovaAynisa <122156745+ToktogulovaAynisa@users.noreply.github.com>
2023-02-15 16:45:38 +0600

    Add files via upload

---
+++ DZ1_Aynisa22/app.js
@@ -0,0 +1,34 @@
+const email = document.querySelector('.email')
+const password = document.querySelector('.password')
+const checkBtn = document.querySelector('.check')
+
+let emailReg = /@/i
+let passReg = /\d{5,}/g
+
+function check () {
+    console.log(passReg.test(password.value));
+}
+
+checkBtn.onclick = check
+
+const moveBlock = document.querySelector('.move__block')
+let x = 0
+let isRepeat = false
+
+function move () {
+    setTimeout(() => {
+        if(x < 1030 && !isRepeat) {
+            x += 20
+            moveBlock.style.left = `${x}px`
+        } else if(x > 0) {
+            isRepeat = true
+            x -= 20
+            moveBlock.style.left = `${x}px`
+        } else {
+            isRepeat = false
+        }
+        move()
+    }, 50)
+}
+
+move()
